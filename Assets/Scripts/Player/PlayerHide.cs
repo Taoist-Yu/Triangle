79622aea69da04517ebbdccfa5feed67a5fe3bd7
MLPu <MLPu@DESKTOP-DVRPMT6>
2019-07-07 03:16:39 +0800

    玩家朝向

--- Assets/Scripts/Player/PlayerHide.cs
+++ Assets/Scripts/Player/PlayerHide.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class PlayerHide : MonoBehaviour
 {
     public bool isHide = false;
-
+    PlayerLight playerLight;
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.transform.tag == "HideSpace")
+        if (isHide == false && other.transform.tag == "HideSpace")
         {
             if (Input.GetKey(KeyCode.E))
             {
+                playerLight.LightDown();
                 //玩家隐藏
                 isHide = true;
             }
             else
             {
+                playerLight.LightUp();
+                print("LightUp");
                 isHide = false;
             }
         }
     }
-
+    void Awake()
+    {
+        playerLight = transform.Find("PlayerLight").GetComponent<PlayerLight>();
+    }
 }
