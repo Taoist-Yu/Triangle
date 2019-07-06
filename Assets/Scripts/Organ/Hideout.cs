258eed8dd661a671b1112e409b453c18ddb49197
Taoist_Yu <294196463@qq.com>
2019-07-06 21:29:19 +0800

    抽象类

---
+++ Assets/Scripts/Organ/Hideout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Hideout : Organ
+{
+
+	public override void OnUse(GameObject player)
+	{
+		
+	}
+
+}
