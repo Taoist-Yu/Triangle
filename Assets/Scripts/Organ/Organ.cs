cc65fdd6ae4eb7a9f9838cfc48e850fa6bed0a30
Taoist_Yu <294196463@qq.com>
2019-07-06 21:29:19 +0800

    机关基类

---
+++ Assets/Scripts/Organ/Organ.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Organ : MonoBehaviour
+{
+
+	public void OnActivate()
+	{
+
+	}
+
+	public abstract void OnUse(GameObject player);
+
+}
